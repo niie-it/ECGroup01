2a4ad78b50397dfb68c34c691469708aa85ee8cd
HIENLTH 2023 <hyhien@gmail.com>
2023-10-25 14:40:16 +0700

    Demo Add to cart

---
+++ MyCommerce/MyCommerce/Controllers/CartController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyCommerce.Data;
+using MyCommerce.Models;
+
+namespace MyCommerce.Controllers
+{
+	public class CartController : Controller
+	{
+		const string KeyCartName = "CART";
+		private readonly MyeStoreContext _ctx;
+		public CartController(MyeStoreContext ctx)
+		{
+			_ctx = ctx;
+		}
+
+		// Hiển thị giỏ hàng
+		public IActionResult Index()
+		{
+			return View(Carts);
+		}
+
+		public List<CartItem> Carts
+		{
+			get
+			{
+				//Lấy từ session ra
+				var gh = HttpContext.Session.Get<List<CartItem>>(KeyCartName);
+				if (gh == null)
+				{
+					gh = new List<CartItem>();
+				}
+				return gh;
+			}
+		}
+		public IActionResult AddToCart(int id, int qty = 1)
+		{
+			var gioHang = Carts;
+
+			//kiểm tra id (MaHH) truyền qua đã nằm trong giỏ hàng hay chưa
+			var item = gioHang.SingleOrDefault(p => p.MaHh == id);
+			if (item != null) //đã có
+			{
+				item.SoLuong += qty;
+			}
+			else
+			{
+				var hangHoa = _ctx.HangHoas.SingleOrDefault(p => p.MaHh == id);
+				if (hangHoa == null)//id không có trong Database
+				{
+					return RedirectToAction("Index", "HangHoas");
+				}
+				item = new CartItem
+				{
+					MaHh = id,
+					SoLuong = qty,
+					TenHh = hangHoa.TenHh,
+					Hinh = hangHoa.Hinh,
+					DonGia = hangHoa.DonGia.Value
+				};
+				//thêm vào giỏ hàng
+				gioHang.Add(item);
+			}
+			//cập nhật session
+			HttpContext.Session.Set(KeyCartName, gioHang);
+			return RedirectToAction("Index");
+		}
+	}
+}
---
+++ MyCommerce/MyCommerce/Models/CartItem.cs
@@ -0,0 +1,13 @@
+﻿namespace MyCommerce.Models
+{
+	public class CartItem
+	{
+		public int MaHh { get; set; }
+		public int SoLuong { get; set; }
+
+		public string TenHh { get; set; }
+		public string? Hinh { get; set; }
+		public double DonGia { get; set; }
+		public double ThanhTien => SoLuong * DonGia;
+	}
+}
